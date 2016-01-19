6f46967bf5defd7f95081eac7d8d70fff262e160
Thomas Lee <lisihan969@gmail.com>
2016-01-20 03:38:52 +0800

    Add four tabs

---
+++ Learn-iOS/MeViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace LearniOS
+{
+	partial class MeViewController : UIViewController
+	{
+		public MeViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
