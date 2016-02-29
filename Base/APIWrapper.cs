8a0567ba29d539352d9f6bd39a48ac77660485b7
walkerning <foxdoraame@gmail.com>
2016-02-29 23:01:08 +0800

    Basic get APIs and basic test wrapper.

---
+++ Base/APIWrapper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Base
+{
+	public static class RestSharpExtensionMethods
+	{
+
+		public static bool IsScuccessStatusCode(this HttpStatusCode responseCode)
+		{
+			int numericResponse = (int)responseCode;
+			return numericResponse >= 200
+				&& numericResponse <= 399;
+		}
+	}
+
+	public class APIWrapper
+	{
+		private const string personalInfoUrl = "/users/me";
+		private const string attendingUrl = "/users/me/attending";
+		private const string attendedUrl = "/users/me/attended";
+		private const string homeworksUrl = "/courses/{courseId}/homeworks";
+		private const string filesUrl = "/courses/{courseId}/files";
+		private const string announcementsUrl = "/courses/{courseId}/announcements";
+
+		private RestClient client;
+		public Dictionary<string, string> CommonHeaders { get; set; } = new Dictionary<string, string>()
+		{
+			{ "Accept", "application/json; charset=utf-8" },
+			{ "Accept-Encoding", "gzip, identity" },
+			{ "User-Agent", "learnAPIWrapper/1.0" }
+		};
+
+		public APIWrapper (string baseUrl, string userName, string password)
+		{
+			client = new RestClient (baseUrl);
+			client.Authenticator = new HttpBasicAuthenticator (userName, password);
+
+		}
+
+		public HttpStatusCode GetPersonalInfo (out string jsonString) =>
+			get (personalInfoUrl, out jsonString);
+
+
+		public HttpStatusCode GetAttending (out string jsonString) =>
+			get (attendingUrl, out jsonString);
+
+
+		public HttpStatusCode GetAttended (out string jsonString) =>
+			get (attendedUrl, out jsonString);
+
+		public HttpStatusCode GetHomeworks (string courseId, out string jsonString) =>
+			get (homeworksUrl, out jsonString, new Dictionary<string, string>() {{"courseId", courseId}});
+
+		public HttpStatusCode GetFiles (string courseId, out string jsonString) =>
+			get (filesUrl, out jsonString, new Dictionary<string, string>() {{"courseId", courseId}});
+
+		public HttpStatusCode GetAnnoucements (string courseId, out string jsonString) =>
+			get (announcementsUrl, out jsonString, new Dictionary<string, string>() {{"courseId", courseId}});
+
+		private HttpStatusCode get (string path, out string jsonString, 
+			Dictionary<string, string> segs = null) {
+			var request = new RestRequest(path, Method.GET);
+
+			if (segs != null) {
+				foreach (var seg in segs) {
+					request.AddUrlSegment (seg.Key, seg.Value.Trim ());
+				}
+			}
+
+			foreach (var header in CommonHeaders) {
+				request.AddHeader (header.Key, header.Value);
+			}
+
+			IRestResponse response = client.Execute(request);
+
+			// Handling errors
+			if (response.ErrorException != null)
+			{
+				const string message = "Error retrieving response.  Check inner details for more info.";
+				throw new ApplicationException(message, response.ErrorException);
+			}
+
+			jsonString = response.Content;
+			return response.StatusCode;
+		}
+	}
+}
+
--- Learn-Console/Program.cs
+++ Learn-Console/Program.cs
@@ -1,28 +1,46 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using RestSharp;
 using Mono.Options;
+
+using Base;
 
 namespace LearnConsole
 {
 	class ParserTest {
 		static int verbosity;
-
+		const string resourceHelpString = 
+			"1. If \"-c\" option is specified: f/files for files; " +
+			"a/announcements for announcements; " +
+			"h/homeworks for homeworks. " +
+			"\n2. Else, i/info for personal info; attending for attending courses; " +
+			"attended for attended courses.";
 		public static void Main (string[] args)
 		{
 			bool show_help = false;
-			List<string> names = new List<string> ();
-			int repeat = 1;
+
+			string userName = "";
+			string password = "";
+			string baseUrl = null;
+			string courseId = null;
+			string resource = null;
 
 			var p = new OptionSet () {
-				{ "n|name=", "the name of someone to greet.",
-					v => names.Add (v) },
-				{ "r|repeat=", "the number of times to repeat the greeting.",
-					(int v) => repeat = v },
 				{ "v", "increase debug message verbosity",
 					v => { if (v != null) ++verbosity; } },
 				{ "h|help",  "show this message and exit", 
 					v => show_help = v != null },
+				{ "u|user=", "the login name to the server",
+					v => userName = v },
+				{ "p|pass=", "the login password to the server",
+					v => password = v },
+				{ "b|base=", "the base url of the proxy server",
+					v => baseUrl = v }, // required
+				{ "c|course=", "the course id",
+					v => courseId = v },
+				{ "r|resource=", "the resource type to fetch:\n" + resourceHelpString,
+					v => resource = v }
 			};
 
 			List<string> extra;
@@ -30,7 +48,6 @@ namespace LearnConsole
 				extra = p.Parse (args);
 			}
 			catch (OptionException e) {
-				Console.Write ("greet: ");
 				Console.WriteLine (e.Message);
 				Console.WriteLine ("Try `greet --help' for more information.");
 				return;
@@ -41,27 +58,70 @@ namespace LearnConsole
 				return;
 			}
 
-			string message;
-			if (extra.Count > 0) {
-				message = string.Join (" ", extra.ToArray ());
-				Debug ("Using new message: {0}", message);
-			}
-			else {
-				message = "Hello {0}!";
-				Debug ("Using default message: {0}", message);
+			// required args
+			if (baseUrl == null) {
+				throw new InvalidOperationException ("Missing required option -r=BASE_URL");
 			}
 
-			foreach (string name in names) {
-				for (int i = 0; i < repeat; ++i)
-					Console.WriteLine (message, name);
+			string jsonString;
+			HttpStatusCode responseCode;
+
+			APIWrapper apiWrapper = new APIWrapper (baseUrl, userName, password);
+
+			Console.WriteLine (resource);
+			Console.WriteLine (courseId);
+			if (courseId == null) {
+				// Default fetch the personal info
+				switch (resource) {
+				case "i":
+				case "info":
+					responseCode = apiWrapper.GetPersonalInfo (out jsonString);
+					break;
+				case "attending":
+					responseCode = apiWrapper.GetAttending (out jsonString);
+					break;
+				case "atttended":
+					responseCode = apiWrapper.GetAttended (out jsonString);
+					break;
+				default:
+					Console.WriteLine (
+						String.Format ("Error: Unrecognized resource type for user infos -- \"{0}\"\n\"-r\" option help:\n{1}",
+							resource,
+							resourceHelpString));
+					return;
+				}
+			} else {
+				switch (resource)
+				{
+				case "h":
+				case "homeworks":
+					responseCode = apiWrapper.GetHomeworks(courseId, out jsonString);
+					break;
+				case "f":
+				case "files":
+					responseCode = apiWrapper.GetFiles(courseId, out jsonString);
+					break;
+				case "a":
+				case "announcements":
+					responseCode = apiWrapper.GetAnnoucements(courseId, out jsonString);;
+					break;
+				default:
+					Console.WriteLine (
+						String.Format ("Error: Unrecognized resource type for course infos -- \"{0}\"\n\"-r\" option help:\n{1}",
+							resource,
+							resourceHelpString));
+					return;
+				}
+				
 			}
+				
+			Console.WriteLine (String.Format ("The response is:\n {0}: {1}\n", responseCode, jsonString));
 		}
 
 		static void ShowHelp (OptionSet p)
 		{
-			Console.WriteLine ("Usage: greet [OPTIONS]+ message");
-			Console.WriteLine ("Greet a list of individuals with an optional message.");
-			Console.WriteLine ("If no message is specified, a generic greeting is used.");
+			Console.WriteLine ("Usage:  Learn-Console.exe [OPTIONS]");
+			Console.WriteLine ("Test the api wrapper.");
 			Console.WriteLine ();
 			Console.WriteLine ("Options:");
 			p.WriteOptionDescriptions (Console.Out);
