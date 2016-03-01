e7f31eb814a6ce427f7daa700ba7d3ca57638225
Nuullll <vfirst218@gmail.com>
2016-03-01 21:29:12 +0800

    Parse commands for console, usint Commandline Parser Library 2.0.275-beta

--- Learn-Console/Program.cs
+++ Learn-Console/Program.cs
@@ -1,138 +1,148 @@
 ﻿using System;
 using System.Net;
 using System.Collections.Generic;
-using RestSharp;
-using Mono.Options;
+using CommandLine;
+using CommandLine.Text;
 
-using Base;
 
 namespace LearnConsole
 {
-	class ParserTest {
-		static int verbosity;
-		const string resourceHelpString = 
-			"1. If \"-c\" option is specified: f/files for files; " +
-			"a/announcements for announcements; " +
-			"h/homeworks for homeworks. " +
-			"\n2. Else, i/info for personal info; attending for attending courses; " +
-			"attended for attended courses.";
-		public static void Main (string[] args)
-		{
-			bool show_help = false;
+    [Verb("update", HelpText = "Update all data.")]
+    public class UpdateOptions {}
 
-			string userName = "";
-			string password = "";
-			string baseUrl = null;
-			string courseId = null;
-			string resource = null;
+    [Verb("annc", HelpText = "Get all Announcements.")]
+    public class AnnouncementOptions 
+    {
+        [Value(0, MetaName = "course", HelpText = "Get Announcements of specific course.")]
+        public string Course { get; set; }
+    }
 
-			var p = new OptionSet () {
-				{ "v", "increase debug message verbosity",
-					v => { if (v != null) ++verbosity; } },
-				{ "h|help",  "show this message and exit", 
-					v => show_help = v != null },
-				{ "u|user=", "the login name to the server",
-					v => userName = v },
-				{ "p|pass=", "the login password to the server",
-					v => password = v },
-				{ "b|base=", "the base url of the proxy server",
-					v => baseUrl = v }, // required
-				{ "c|course=", "the course id",
-					v => courseId = v },
-				{ "r|resource=", "the resource type to fetch:\n" + resourceHelpString,
-					v => resource = v }
-			};
+    [Verb("file", HelpText = "Get all files.")]
+    public class FileOptions
+    {
+        [Value(0, MetaName = "course", HelpText = "Get files of specific course.")]
+        public string Course { get; set; }
+    }
 
-			List<string> extra;
-			try {
-				extra = p.Parse (args);
-			}
-			catch (OptionException e) {
-				Console.WriteLine (e.Message);
-				Console.WriteLine ("Try `greet --help' for more information.");
-				return;
-			}
+    [Verb("hw", HelpText = "Get all homeworks.")]
+    public class HomeworkOptions
+    {
+        [Value(0, MetaName = "course", HelpText = "Get homeworks of specific course.")]
+        public string Course { get; set; }
+    }
 
-			if (show_help) {
-				ShowHelp (p);
-				return;
-			}
+    [Verb("profile", HelpText = "Get user profile.")]
+    public class ProfileOptions {}
 
-			// required args
-			if (baseUrl == null) {
-				throw new InvalidOperationException ("Missing required option -r=BASE_URL");
-			}
+    [Verb("info", HelpText = "Get information of attending courses.")]
+    public class InfoOptions 
+    {
+        [Option('a', "all", HelpText = "Get information of all courses.")]
+        public bool All { get; set; }
+    }
 
-			string jsonString;
-			HttpStatusCode responseCode;
+    [Verb("ls", HelpText = "Get list of attending courses.")]
+    public class LsOptions
+    {
+        [Option('a', "all", HelpText = "Get list of all courses.")]
+        public bool All { get; set; }
+    }
 
-			APIWrapper apiWrapper = new APIWrapper (baseUrl, userName, password);
 
-			Console.WriteLine (resource);
-			Console.WriteLine (courseId);
-			if (courseId == null) {
-				// Default fetch the personal info
-				switch (resource) {
-				case "i":
-				case "info":
-					responseCode = apiWrapper.GetPersonalInfo (out jsonString);
-					break;
-				case "attending":
-					responseCode = apiWrapper.GetAttending (out jsonString);
-					break;
-				case "atttended":
-					responseCode = apiWrapper.GetAttended (out jsonString);
-					break;
-				default:
-					Console.WriteLine (
-						String.Format ("Error: Unrecognized resource type for user infos -- \"{0}\"\n\"-r\" option help:\n{1}",
-							resource,
-							resourceHelpString));
-					return;
-				}
-			} else {
-				switch (resource)
-				{
-				case "h":
-				case "homeworks":
-					responseCode = apiWrapper.GetHomeworks(courseId, out jsonString);
-					break;
-				case "f":
-				case "files":
-					responseCode = apiWrapper.GetFiles(courseId, out jsonString);
-					break;
-				case "a":
-				case "announcements":
-					responseCode = apiWrapper.GetAnnoucements(courseId, out jsonString);;
-					break;
-				default:
-					Console.WriteLine (
-						String.Format ("Error: Unrecognized resource type for course infos -- \"{0}\"\n\"-r\" option help:\n{1}",
-							resource,
-							resourceHelpString));
-					return;
-				}
-				
-			}
-				
-			Console.WriteLine (String.Format ("The response is:\n {0}: {1}\n", responseCode, jsonString));
-		}
+    public class LearnConsole
+    {
+        public static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<
+                UpdateOptions, AnnouncementOptions, FileOptions,
+                HomeworkOptions, ProfileOptions, InfoOptions,
+                LsOptions>(args).MapResult(
+                    (UpdateOptions opts) => Update(opts),
+                    (AnnouncementOptions opts) => Announcement(opts),
+                    (FileOptions opts) => File(opts),
+                    (HomeworkOptions opts) => Homework(opts),
+                    (ProfileOptions opts) => Profile(opts),
+                    (InfoOptions opts) => Info(opts),
+                    (LsOptions opts) => Ls(opts),
+                    errs => 1);
+        }
 
-		static void ShowHelp (OptionSet p)
-		{
-			Console.WriteLine ("Usage:  Learn-Console.exe [OPTIONS]");
-			Console.WriteLine ("Test the api wrapper.");
-			Console.WriteLine ();
-			Console.WriteLine ("Options:");
-			p.WriteOptionDescriptions (Console.Out);
-		}
+        public static int Update(UpdateOptions opts)
+        {
+            Console.WriteLine("Update command parsed.");
+            return 0;
+        }
 
-		static void Debug (string format, params object[] args)
-		{
-			if (verbosity > 0) {
-				Console.Write ("# ");
-				Console.WriteLine (format, args);
-			}
-		}
-	}
+        public static int Announcement(AnnouncementOptions opts)
+        {
+            if (opts.Course == null)
+            {
+                Console.WriteLine("Announcements of course: all");
+            }
+            else
+            {
+                Console.WriteLine("Announcements of course: {0}", opts.Course);
+
+            }
+            return 0;
+        }
+
+        public static int File(FileOptions opts)
+        {
+            if (opts.Course == null)
+            {
+                Console.WriteLine("Files of course: all");
+            }
+            else
+            {
+                Console.WriteLine("Files of course: {0}", opts.Course);
+            }
+            return 0;
+        }
+
+        public static int Homework(HomeworkOptions opts)
+        {
+            if (opts.Course == null)
+            {
+                Console.WriteLine("Homeworks of course: all");
+            }
+            else
+            {
+                Console.WriteLine("Homeworks of course: {0}", opts.Course);
+            }
+            return 0;
+        }
+
+        public static int Profile(ProfileOptions opts)
+        {
+            Console.WriteLine("Profile command parsed.");
+            return 0;
+        }
+
+        public static int Info(InfoOptions opts)
+        {
+            if (opts.All)
+            {
+                Console.WriteLine("Information of all courses.");
+            }
+            else
+            {
+                Console.WriteLine("Information of attending courses.");
+            }
+            return 0;
+        }
+
+        public static int Ls(LsOptions opts)
+        {
+            if (opts.All)
+            {
+                Console.WriteLine("List of all courses.");
+            }
+            else
+            {
+                Console.WriteLine("List of attending courses.");
+            }
+            return 0;
+        }
+    }
 }
