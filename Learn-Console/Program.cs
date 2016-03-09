1b14b4f4cacd8e31f3de7c5669cd0b207ead6470
walkerning <foxdoraame@gmail.com>
2016-03-09 23:24:39 +0800

    Add Update and Info.
    Modify definition of option classes.

--- Learn-Console/Program.cs
+++ Learn-Console/Program.cs
@@ -1,162 +1,176 @@
 ﻿using System;
+using System.Collections.Generic;
 using CommandLine;
 
 using Base;
 
 namespace LearnConsole
 {
-	public class CommonOptions {
-		[Option('u', "user", Required = true, HelpText = "The username.")]
+
+	[Verb ("update", HelpText = "Update all data.")]
+	public class UpdateOptions
+	{
+		[Option ('u', "user", Required = true, HelpText = "The username.")]
 		public string User { get; set; }
 
-		[Option('p', "password", Required = true, HelpText = "The password.")]
+		[Option ('p', "password", Required = true, HelpText = "The password.")]
 		public string Password { get; set; }
 
-		[Option('s', "server", HelpText = "The server address.", Default = "http://localhost")]
+		[Option ('s', "server", HelpText = "The server address.", Default = "http://localhost")]
 		public string Server { get; set; }
+
+		[Option ('v', "verbose", HelpText = "Verbosely print out the update informations.")]
+		public bool Verbose { get; set; }
 	}
 
-    [Verb("update", HelpText = "Update all data.")]
-	public class UpdateOptions: CommonOptions {}
+	[Verb ("annc", HelpText = "Get all Announcements.")]
+	public class AnnouncementOptions
+	{
+		[Value (0, Required = true, MetaName = "course", HelpText = "Get Announcements of specific course.")]
+		public string Course { get; set; }
+	}
 
-    [Verb("annc", HelpText = "Get all Announcements.")]
-	public class AnnouncementOptions: CommonOptions
-    {
-        [Value(0, Required = true, MetaName = "course", HelpText = "Get Announcements of specific course.")]
-        public string Course { get; set; }
-    }
+	[Verb ("file", HelpText = "Get all files.")]
+	public class FileOptions
+	{
+		[Value (0, Required = true, MetaName = "course", HelpText = "Get files of specific course.")]
+		public string Course { get; set; }
+	}
 
-    [Verb("file", HelpText = "Get all files.")]
-    public class FileOptions: CommonOptions
-    {
-        [Value(0, Required = true, MetaName = "course", HelpText = "Get files of specific course.")]
-        public string Course { get; set; }
-    }
+	[Verb ("hw", HelpText = "Get all homeworks.")]
+	public class HomeworkOptions
+	{
+		[Value (0, Required = true, MetaName = "course", HelpText = "Get homeworks of specific course.")]
+		public string Course { get; set; }
+	}
 
-    [Verb("hw", HelpText = "Get all homeworks.")]
-    public class HomeworkOptions: CommonOptions
-    {
-        [Value(0, Required = true, MetaName = "course", HelpText = "Get homeworks of specific course.")]
-        public string Course { get; set; }
-    }
+	[Verb ("info", HelpText = "Show course info.")]
+	public class CourseInfoOptions
+	{
+		[Value (0, Required = true, MetaName = "course", HelpText = "Get info of specific course.")]
+		public string Course { get; set; }
+	}
 
-    [Verb("profile", HelpText = "Get user profile.")]
-    public class ProfileOptions: CommonOptions {}
+	[Verb ("profile", HelpText = "Get user profile.")]
+	public class ProfileOptions
+	{
 
-    [Verb("attend", HelpText = "List of all courses.")]
-    public class AttendOptions: CommonOptions
-    {
-    	[Option("semester", HelpText = "--semester=now: List of courses in this semester.")]
-    	public string semester { get; set; }
+	}
 
-    	[Option("detail", HelpText = "Detailed information of courses.")]
-    	public bool detail { get; set; }
-    }
+	[Verb ("attend", HelpText = "List of all courses.")]
+	public class AttendOptions
+	{
+		[Option ("semester", HelpText = "--semester=now: List of courses in this semester.")]
+		public string semester { get; set; }
+
+		[Option ("detail", HelpText = "Detailed information of courses.")]
+		public bool detail { get; set; }
+	}
 
 
-    public class LearnConsole
-    {
-		public static APIWrapper GetApiWrapper (CommonOptions options) {
+	public class LearnConsole
+	{
+		public static APIWrapper GetApiWrapper (UpdateOptions options)
+		{
 			LogInfo ("Using proxy server", options.Server);
 			return new APIWrapper (options.Server, options.User, options.Password);
 		}
 
-		public static UpdateAgent GetUpdateAgent (CommonOptions options) {
+		public static UpdateAgent GetUpdateAgent (UpdateOptions options)
+		{
 			LogInfo ("Using proxy server", options.Server);
 			return new UpdateAgent (options.Server, options.User, options.Password);
 		}
 
-		public static int Main(string[] args) {
+		public static int Main (string[] args)
+		{
 			return Parser.Default.ParseArguments<
                 UpdateOptions, AnnouncementOptions, FileOptions,
-				HomeworkOptions, ProfileOptions, AttendOptions> (args).MapResult (
+				HomeworkOptions, ProfileOptions, AttendOptions,
+				CourseInfoOptions> (args).MapResult (
 				(UpdateOptions opts) => Update (opts),
 				(AnnouncementOptions opts) => Announcement (opts),
 				(FileOptions opts) => File (opts),
 				(HomeworkOptions opts) => Homework (opts),
 				(ProfileOptions opts) => Profile (opts),
 				(AttendOptions opts) => Attend (opts),
+				(CourseInfoOptions opts) => Info (opts),
 				errs => 1);
-        }
+		}
 
-        public static int Update(UpdateOptions opts)
-        {
-            Console.WriteLine ("Update command parsed.");
+		public static int Update (UpdateOptions opts)
+		{
+			Console.WriteLine ("Update command parsed.");
 			var status = GetUpdateAgent (opts).UpdateAll ();
 			if (!status) {
 				Console.WriteLine ("Some update failed.");
 			}
-			return status?0:1;
-        }
+			return status ? 0 : 1;
+		}
 
-        public static int Announcement(AnnouncementOptions opts)
-        {
-			string jsonString;
-			var status = GetApiWrapper (opts).GetAnnoucements (opts.Course, out jsonString);
-			LogInfo (String.Format("Annoucements of course: {0}", opts.Course), 
-				String.Format("{0}: {1}", status, jsonString));
-            return 0;
-        }
-
-        public static int File(FileOptions opts)
-        {
-			string jsonString;
-			var status = GetApiWrapper (opts).GetFiles (opts.Course, out jsonString);
-			LogInfo (String.Format("Files of course: {0}", opts.Course), 
-				String.Format("{0}: {1}", status, jsonString));
+		public static int Announcement (AnnouncementOptions opts)
+		{
+			// Todo: Get announcements from announcements by courseId view
 			return 0;
-        }
+		}
 
-        public static int Homework(HomeworkOptions opts)
-        {
-			string jsonString;
-			var status = GetApiWrapper (opts).GetHomeworks (opts.Course, out jsonString);
-			LogInfo (String.Format("Homeworks of course: {0}", opts.Course), 
-				String.Format("{0}: {1}", status, jsonString));
+		public static int File (FileOptions opts)
+		{
+			// Todo: Get files from files by courseId view
 			return 0;
-        }
+		}
 
-        public static int Profile(ProfileOptions opts)
-        {
-			string jsonString;
-			var status = GetApiWrapper (opts).GetProfile (out jsonString);
-			LogInfo (String.Format("Profile of user {0}", opts.User), 
-				String.Format("{0}: {1}", status, jsonString));
+		public static int Homework (HomeworkOptions opts)
+		{
+			// Todo: Get homeworks from homeworks by courseId view
 			return 0;
-        }
+		}
 
-        public static int Attend(AttendOptions opts)
-        {
-        	if (opts.semester == null)
-        	{
-        		if (opts.detail)
-        		{
-        			Console.WriteLine("Detailed information of all courses.");
-        		}
-        		else 
-        		{
-        			Console.WriteLine("List of all courses.");
-        		}
-        	}
-        	else if (opts.semester == "now") 
-        	{
-        		if (opts.detail)
-        		{
-        			Console.WriteLine("Detailed information of all courses in this semester.");
-        		}
-        		else 
-        		{
-        			Console.WriteLine("List of all courses in this semester.");
-        		}
-        	}
-        	return 0;
-        }
+		public static int Info (CourseInfoOptions opts) 
+		{
+			var course = new Course(new Dictionary<string, object>() {
+				{"id", opts.Course}
+			});
+			Console.WriteLine (course);
+			return 0;
+		}
+
+		public static int Profile (ProfileOptions opts)
+		{
+			return 0;
+		}
 			
-		public static void LogInfo(string title, string content)
+		public static int Attend (AttendOptions opts)
+		{
+			string semester = opts.semester ?? "all";
+			List<Course> courses = new List<Course> (); // just for compiling now
+
+			switch (semester) {
+			case "all":
+				// Todo: get courses from all-course query
+				break;
+			case "now":
+				// Todo: get courses form now-course query
+				break;
+			default:
+				Console.WriteLine ("Unsupported semester argument \"{0}\".", semester);
+				break;
+			}
+
+			if (opts.detail) {
+				// All informations
+			} else {
+				// Only name and id are printed
+				courses.ForEach(course => Console.WriteLine (course));
+			}
+			return 0;
+		}
+
+		public static void LogInfo (string title, string content)
 		{
 			Console.WriteLine (title + ":");
 			Console.WriteLine ("\t" + content);
 		}
 
-    }
+	}
 }
